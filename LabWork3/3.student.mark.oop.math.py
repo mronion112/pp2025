2287445709509fe5146a9f903585143e4e47501b
mronion112 <mr.onion112@gmail.com>
2025-12-11 16:52:07 +0700

    Labwork3 commit done

--- LabWork3/3.student.mark.oop.math.py
+++ LabWork3/3.student.mark.oop.math.py
@@ -1,139 +1,176 @@
 import numpy as np
 import math
 
-class Class:
-    def __init__(self, numberStudents):
-        self._numberStudents = numberStudents
 
-    def _getNumberStudents(self):
-        return self._numberStudents
+class Entity():
+    def __init__(self, id, name):
+        self.__id = id
+        self.__name = name
 
-    def _setNumberStudents(self, value):
-        self._numberStudents = value
+    def getId(self):
+        return self.__id
+
+    def setId(self, id):
+        self.__id = id
+
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
 
 
-class Student:
+class Student(Entity):
     def __init__(self, id, name, DoB):
-        self._id = id
-        self._name = name
-        self._DoB = DoB
+        super().__init__(id, name)
+        self.__DoB = DoB
+        self.__marks = {}  # Key: CourseID, Value: Score
+        self.__gpa = 0.0
 
-    def _getId(self):
-        return self._id
+    def calculate_gpa(self):
+        scores = list(self.__marks.values())
 
-    def _setId(self, value):
-        self._id = value
-
-    def _getName(self):
-        return self._name
-
-    def _setName(self, value):
-        self._name = value
-
-    def _getDoB(self):
-        return self._DoB
-
-    def _setDoB(self, value):
-        self._DoB = value
-
-
-class Course:
-    def __init__(self, id, name, listStudents):
-        self._id = id
-        self._name = name
-        self._listStudents = listStudents
-
-    def _getId(self):
-        return self._id
-
-    def _setId(self, value):
-        self._id = value
-
-    def _getName(self):
-        return self._name
-
-    def _setName(self, value):
-        self._name = value
-
-    def _getlistStudents(self):
-        return self._listStudents
-
-    def _setlistStudents(self, value):
-        self._listStudents = value
-
-
-class ListCourse:
-    def __init__(self, numberCourses, listCourses):
-        self._numberCourses = numberCourses
-        self._listCourses = listCourses
-
-    def _getNumberCourses(self):
-        return self._numberCourses
-
-    def _setNumberCourses(self, value):
-        self._numberCourses = value
-
-    def _getlistCourses(self):
-        return self._listCourses
-
-    def _setlistCourses(self, value):
-        self._listCourses = value
-
-    def markScoreWithCourceId(self, courseId):
-        listStudentMark = []
-        course_found = None
-
-        for course in self._listCourses:
-            if course._id == courseId:
-                course_found = course
-                break
-
-        if course_found:
-            students = course_found._getlistStudents()
-            print(f"\nNhập điểm cho khóa học: {course_found._name} (ID: {courseId})")
-
-            for student in students:
-                while True:
-                    try:
-                        markNumber = math.floor(float(input(f"Điểm của sinh viên {student._name} (ID: {student._id}): ")))
-                        if 0 <= markNumber <= 20:
-                            break
-                        else:
-                            print("Điểm phải nằm trong khoảng 0-20. Vui lòng nhập lại.")
-                    except ValueError:
-                        print("Nhập liệu không hợp lệ. Vui lòng nhập một số.")
-
-
-                listStudentMark.append(f"Sinh viên {student._name} (ID: {student._id}) có điểm là {markNumber}")
-
-            return listStudentMark  # Trả về danh sách điểm
+        if len(scores) > 0:
+            self.__gpa = np.mean(np.array(scores))
         else:
-            print(f"Không tìm thấy khóa học với ID: {courseId}")
+            self.__gpa = 0.0
+
+    def getDoB(self):
+        return self.__DoB
+
+    def setDoB(self, DoB):
+        self.__DoB = DoB
+
+    def getMarks(self):
+        return self.__marks
+
+    def setMarks(self, course_id, marks):
+        self.__marks[course_id] = marks
+
+    def getGPA(self):
+        return self.__gpa
+
+    def setGPA(self, gpa):
+        self.__gpa = gpa
+
+    def __str__(self):
+        return f"Student id = {super().getId()}, name = {super().getName()}, DoB = {self.__DoB}, GPA = {self.__gpa:.2f}"
+
+
+class Course(Entity):
+    def __init__(self, id, name, listStudents):
+        super().__init__(id, name)
+        self.__listStudent = listStudents
+
+    def getListStudents(self):
+        return self.__listStudent
+
+    def __str__(self):
+        return f"Course id = {super().getId()}, name = {super().getName()}"
+
+
+class CourseManager():
+    def __init__(self, numberOfCourses, listCourses):
+        self.__numberOfCourses = numberOfCourses
+        self.__listCourses = listCourses
+
+    def markStudentScoreWithCourseId(self):
+        listStudentMark = []
+        courseFound = None
+
+        try:
+            idCourse = int(input("Id course to mark: "))
+        except ValueError:
+            print("Invalid ID")
             return []
 
+        for course in self.__listCourses:
+            if course.getId() == idCourse:
+                courseFound = course
+                break
 
-class1 = Class(3)
+        if courseFound != None:
+            print(f"Marking for course: {courseFound.getName()}")
 
-studen1 = Student(1, "HA", "11/11/2020")
-student2 = Student(2, "TRUNG", "12/12/2020")
-student3 = Student(3, "NAM", "13/13/2020")
+            for student in courseFound.getListStudents():
+                try:
+                    numberScore = float(input(f"Score for student {student.getName()} (ID {student.getId()}): "))
 
-listStudent = [studen1, student2, student3]
+                    floorScore = math.floor(numberScore * 10) / 10
 
-course1 = Course(1, "Calculus", listStudent)
-course2 = Course(2, "Linear", listStudent)
-course3 = Course(3, "Physic", listStudent)
+                    student.setMarks(idCourse, floorScore)
 
-Couses = [course1, course2, course3]
+                    student.calculate_gpa()
 
-listCourses = ListCourse(len(Couses), Couses)
+                    listStudentMark.append(f"Student {student.getName()} mark is {floorScore}")
+                except ValueError:
+                    print("Invalid score input")
+
+        else:
+            print(f"Can't find the course id = {idCourse}")
+
+        return listStudentMark
+
+    def sortStudentByGPA(self, listStudents):
+        for student in listStudents:
+            student.calculate_gpa()
+
+        listStudents.sort(key=lambda x: x.getGPA(), reverse=True)
 
 
-listStudentMark = listCourses.markScoreWithCourceId(1)
 
-if listStudentMark:
-    for record in listStudentMark:
-        print(record)
-else:
-    print("Không có điểm nào được nhập hoặc khóa học không tồn tại.")
+def createListStudent():
+    listStudents = []
+    try:
+        numberStudents = int(input("Number of Students: "))
+        for i in range(numberStudents):
+            idStudent = int(input(f"Id student {i + 1}: "))
+            nameStudent = input("Name Student: ")
+            DoBStudent = input("DoB Student: ")
+            listStudents.append(Student(idStudent, nameStudent, DoBStudent))
+            print("\n")
+    except ValueError:
+        print("Invalid input")
+    return listStudents
 
+
+def printList(my_list):
+    for item in my_list:
+        print(item)
+
+
+def createListCourses(listStudents):
+    listCourses = []
+    try:
+        numberCourses = int(input("Number of Courses: "))
+        for i in range(numberCourses):
+            idCourse = int(input(f"Id course {i + 1}: "))
+            nameCourse = input("Name Course: ")
+            listCourses.append(Course(idCourse, nameCourse, listStudents))
+            print("\n")
+    except ValueError:
+        print("Invalid input")
+    return listCourses
+
+
+
+listStudents = createListStudent()
+printList(listStudents)
+
+print("\nCreate listCourse")
+listCourses = createListCourses(listStudents)
+printList(listCourses)
+
+courseManager = CourseManager(len(listCourses), listCourses)
+
+while True:
+    choice = input("\nDo you want to mark a course? (y/n): ")
+    if choice.lower() != 'y':
+        break
+    result = courseManager.markStudentScoreWithCourseId()
+    printList(result)
+
+# Sắp xếp và in ra kết quả GPA
+print("\nSorting Students by GPA")
+courseManager.sortStudentByGPA(listStudents)
+printList(listStudents)
